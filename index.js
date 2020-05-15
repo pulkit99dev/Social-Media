c09f985f4ce5fa4ad2af3f698287a9aa32e24222
Pulkit Nagpal <pulkitnagpal987654321@gmail.com>
2020-05-15 23:28:30 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const app= express();
+const port= 9000;
+
+app.use('/', require('./routes/index'))
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error is running the server: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`)
+})
